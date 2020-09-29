acf5b4e8270e11108fe8fefb01719fb1cbb70f5f
manuel högler <hoeglermanuel@gmail.com>
2020-09-29 12:58:33 +0200

    RationalCalculator class finished with 'erben' and override the methods

---
+++ src/htl/grieskirchen/RationalCalculator.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package htl.grieskirchen;
+
+/**
+ *
+ * @author hoegl
+ */
+public class RationalCalculator extends AbstractCalculator {
+
+    public RationalCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+      return add.calc(a, b);
+        
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+     return subtract.calc(a, b);
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+       return multiply.calc(a, b);
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return divide.calc(a, b);
+    }
+    
+}
