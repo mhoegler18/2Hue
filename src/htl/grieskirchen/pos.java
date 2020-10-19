cd2340d055d20051da6567bd02cbf3c70e4522c7
manuel högler <hoeglermanuel@gmail.com>
2020-10-19 23:00:04 +0200

    15th Exercise 2 finsihed

--- src/htl/grieskirchen/pos.java
+++ src/htl/grieskirchen/pos.java
@@ -6,6 +6,9 @@
 
 package htl.grieskirchen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author hoegl
@@ -15,8 +18,33 @@ public class pos {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    String name;
+
+    public pos(String name) {
+        this.name = name;
     }
     
+    static ArrayList<String> liste = new ArrayList<>();
+    public static void ausgeben(){
+        liste.add("HalloJavamitForEach");
+        liste.forEach(System.out::println);
+        
+    }
+    public static void main(String[] args) {
+        ausgeben();
+        printTable();
+    }
+    public static void printTable(){
+      Printable rC = (List<String>lis) -> {
+         
+          for (int i = 0; i < liste.size(); i++) {
+             
+              System.out.println(liste.toString());
+          }
+          
+      };
+      rC.print(liste);
+      
+  }
+           
 }
